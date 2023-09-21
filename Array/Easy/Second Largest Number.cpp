c2a7bcfcc3aa812e66176f8681fd617b554149f9
Ashutosh <106524453+Ashutosh741@users.noreply.github.com>
2023-09-21 19:59:15 +0530

    Update Second Largest Number.cpp

--- Array/Easy/Second Largest Number.cpp
+++ Array/Easy/Second Largest Number.cpp
@@ -24,3 +24,32 @@ vector<int> getSecondOrderElements(int n, vector<int> a) {
   return {maxi2,mini2};
 
 }
+
+
+
+// single traversal
+
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+  // Write your code here.
+  int maxi = a[0], maxi2 = a[1];
+    if(maxi < maxi2)swap(maxi2,maxi);
+      int mini = a[0],mini2 = a[1];
+    if(mini > mini2)swap(mini2,mini);
+  for (int i = 1; i < n; i++){
+      if(maxi < a[i]){
+          maxi2 = maxi;
+          maxi = a[i];
+      }
+      if(a[i] != maxi)
+      maxi2 = max(maxi2,a[i]);
+            if(mini > a[i]){
+          mini2 = mini;
+          mini = a[i];
+      }
+      if(mini != a[i])
+      mini2 = min(mini2,a[i]);
+  }
+
+  return {maxi2,mini2};
+
+}
