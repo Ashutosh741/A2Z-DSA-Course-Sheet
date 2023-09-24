ae722fd7e8c34d803f5095290f05456189fd9c36
Ashutosh <106524453+Ashutosh741@users.noreply.github.com>
2023-09-24 21:52:44 +0530

    Update 283. Move Zeroes.cpp

--- Array/Easy/283. Move Zeroes.cpp
+++ Array/Easy/283. Move Zeroes.cpp
@@ -17,3 +17,24 @@ public:
         }
     }
 };
+
+
+// updated code
+
+
+vector<int> moveZeros(int n, vector<int> nums) {
+    // Write your code here.
+    int i=0,j=1;
+    while(j < n && i < j){
+        if(nums[i]==0 && nums[j] != 0){
+            swap(nums[i],nums[j]);
+            i++,j++;
+        }else if(nums[i] == 0 && nums[j] == 0){
+            j++;
+        }else{
+            i++,j++;
+        }
+    }
+    return nums;
+
+}
