f2acdf2dc72941d6062393093d43b77adce41c9e
Ashutosh <106524453+Ashutosh741@users.noreply.github.com>
2023-10-02 09:39:40 +0530

    Create 121. Best Time to Buy and Sell Stock.cpp

---
+++ Array/Medium/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi = 0, mini = prices[0],n = prices.size();
+        for(int i=0;i<n;i++){
+            if(prices[i] < mini)mini = prices[i];
+            else if(mini < prices[i]){
+                maxi = max(maxi, prices[i]-mini);
+            }
+        }
+        return maxi;
+    }
+};
